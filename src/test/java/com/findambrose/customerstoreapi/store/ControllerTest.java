018ece5f43a83ca7cfc0738299dee36d712f1609
findambrose <ambkyusya@gmail.com>
2021-02-15 16:46:00 +0300

    Initial commit on Store Tests

---
+++ src/test/java/com/findambrose/customerstoreapi/store/ControllerTest.java
@@ -0,0 +1,40 @@
+package com.findambrose.customerstoreapi.store;
+
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class ControllerTest {
+
+
+    @InjectMocks
+    StoreController storeController;
+    @Mock
+    StoreRepository storeService;
+
+    @Test
+    public void testGetAll(){
+
+    }
+
+    @Test
+    public void testGetOne(){
+
+    }
+
+    @Test
+    public void testDelete(){
+
+    }
+
+    @Test
+    public void testupdate(){
+
+    }
+
+    @Test
+    public void testSave(){
+
+    }
+}
